624bc209eefa017ffae4bf0bf5f7e36a88d4b07a
muthuri-dev <muthurikennedy082@gmail.com>
2024-01-31 14:49:57 +0300

    Update global.css and index.html files

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ import Item from "./modules/shop.js";
 document.getElementById("test").innerHTML = message();
 
 // the classes
-const newItem = new Item("pen", 500);
+const newItem = new Item("pensils", 500);
+const item1 = new Item("books", 700);
+const item2 = new Item("laptop", 2100);
+const item3 = new Item("set", 450);
+const item4 = new Item("table", 1570);
 
-document.querySelector("#shop").innerHTML =
-  "The original price is " + newItem.getDiscount();
+//document.querySelector("#shop").innerHTML = "$ " + newItem.getDiscount();
+document.querySelector("#shop").innerHTML = "$ " + item2.getDiscount();
+document.querySelector("#name").innerHTML = newItem.name;
